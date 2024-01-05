c3570c896dc949c2a7759bec2dcdc20b61f23a6d
hinsoned <102074110+hinsoned@users.noreply.github.com>
2024-01-04 19:23:53 -0500

    Jumping, ground, gravity

--- pygame_practice.py
+++ pygame_practice.py
@@ -17,12 +17,13 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 #create a font. None will be a default pygame font
 test_font = pygame.font.Font('font/Pixeltype.ttf',50)
+game_active = True
 
 # this is a surface to go on the display surface. Takes a file path to an image
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 # text must be rendered to a surface then blitted on to the display surface
-text_surface = test_font.render('My Game', False, 'Black')
+text_surface = test_font.render('My Game', False, (64, 64, 64))
 text_rect = text_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -30,11 +31,14 @@ snail_rect = snail_surface.get_rect(bottomright = (600, 300))
 
 player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80, 300))
+player_gravity = 0
 
 #this loop is always true so it must be broken from the inside
 #the entire game runs in this. It is what keeps the screen created
 #above from just disappearing imediately.
 while True:
+
+    mouse_pos = pygame.mouse.get_pos()
     #this is the event loop or event handler
     #it looks for input and handles it
     for event in pygame.event.get():
@@ -43,35 +47,47 @@ while True:
             pygame.quit()
             # exit closes the code running the while loop
             exit()
-        if event.type == pygame.MOUSEMOTION:
-            if player_rect.collidepoint(mouse_pos):
-                print('collision')
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(mouse_pos):
+                    player_gravity = -20
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                        player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game_active = True
+                    snail_rect.left = 800
+    
     #draw and update all elements here
             
-    #now draw the surface on the display surface 
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,300))
-    pygame.draw.rect(screen, 'Pink', text_rect)
-    pygame.draw.rect(screen, 'Pink', text_rect, 30)
+    if game_active:
+        #now draw the surface on the display surface 
+        screen.blit(sky_surface, (0,0))
+        screen.blit(ground_surface, (0,300))
+        pygame.draw.rect(screen, '#c0e8ec' , text_rect)
+        pygame.draw.rect(screen, '#c0e8ec', text_rect, 10)
 
-    screen.blit(text_surface, text_rect)
-    snail_rect.left -= 4
-    if snail_rect.right <= 0:
-        snail_rect.left = 800
-    screen.blit(snail_surface, snail_rect)
+        screen.blit(text_surface, text_rect)
+        snail_rect.left -= 4
+        if snail_rect.right <= 0:
+            snail_rect.left = 800
+        screen.blit(snail_surface, snail_rect)
 
-    pygame.draw.ellipse(screen, 'Orange', pygame.Rect(50, 200, 100, 100))
+        #player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+        screen.blit(player_surface, player_rect)
 
-    screen.blit(player_surface, player_rect)
-    
-    #check for collisions between rects. Returns 0 or 1 for collision
-    #pygame checks every frame!
-    #if player_rect.colliderect(snail_rect):
-    #    print('collision')
-
-    mouse_pos = pygame.mouse.get_pos()
-    #if player_rect.collidepoint(mouse_pos):
-    #    print(pygame.mouse.get_pressed())
+        # collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('Blue')
 
     #this updates the display surface each time the code reads it
     pygame.display.update()
